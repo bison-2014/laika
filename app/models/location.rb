32736787e9199ae4de7d28731ee7667400d32c05
Lele Duan <duanstar@gmail.com>
2015-02-13 18:08:38 -0600

    added some fixes for models

--- app/models/location.rb
+++ app/models/location.rb
@@ -2,9 +2,16 @@ class Location
   include Mongoid::Document
   include Mongoid::Timestamps
 
-  field :latitude, type: Float
-  field :longitude, type: Float
+  field :coordinates, type: Array
   field :name, type: String
 
   has_many :attractions
+
+  def latitude
+    self.coordinate[0]
+  end
+
+  def longitude
+    self.coordinate[1]
+  end
 end
