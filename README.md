459c80699b0f156fa173d078d4a2d1de47de90a3
Glenna Mowry <glennamowry@gmail.com>
2015-02-15 17:09:02 -0600

    minor changes to user model for validation editing, potential category search filter added but need current_user data

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # laika
 An app that sends you places
+
+Group Members
+* Lele Duan
+* Glenna Mowry
+* Nell Taylor
+* Nicole McCabe
+
+
--- app/controllers/maps_controller.rb
+++ app/controllers/maps_controller.rb
@@ -11,4 +11,20 @@ class MapsController < ApplicationController
     render json: { attractions: @attractions }
   end
 
+# need to set current_user in order for this to work
+  # def search
+  #   geometry = params['features'][0]['geometry']
+  #   if current_user.interests.count > 0
+  #     @matching_attractions = user.interests.each { |categ| categ.attractions }
+  #     @attractions = @matching_attractions.search_within(geometry).all.to_a
+  #   else
+  #     @attractions = Attraction.search_within(geometry).all.to_a
+  #   end
+  #   render json: { attractions: @attractions }
+  # end
+
 end
+
+
+#from console this works: u.interests.each { |interest| p interest.attractions }
+#displays all the business names: u.interests.each { |intr| p intr.attractions.each { |attr| p attr.name } }
