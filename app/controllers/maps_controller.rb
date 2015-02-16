6c7b310946290e5023e06aef0ae4a3d3b459414f
Glenna Mowry <glennamowry@gmail.com>
2015-02-15 18:20:25 -0600

    added in category search filter

--- app/controllers/maps_controller.rb
+++ app/controllers/maps_controller.rb
@@ -5,23 +5,23 @@ class MapsController < ApplicationController
   def index
   end
 
-  def search
-    geometry = params['features'][0]['geometry']
-    @attractions = Attraction.search_within(geometry).all.to_a
-    render json: { attractions: @attractions }
-  end
-
-# need to set current_user in order for this to work
   # def search
   #   geometry = params['features'][0]['geometry']
-  #   if current_user.interests.count > 0
-  #     @matching_attractions = user.interests.each { |categ| categ.attractions }
-  #     @attractions = @matching_attractions.search_within(geometry).all.to_a
-  #   else
-  #     @attractions = Attraction.search_within(geometry).all.to_a
-  #   end
+  #   @attractions = Attraction.search_within(geometry).all.to_a
   #   render json: { attractions: @attractions }
   # end
+
+# need to set current_user in order for this to work
+  def search
+    geometry = params['features'][0]['geometry']
+    if current_user.interests.count > 0
+      @matching_attractions = user.interests.each { |categ| categ.attractions }
+      @attractions = @matching_attractions.search_within(geometry).all.to_a
+    else
+      @attractions = Attraction.search_within(geometry).all.to_a
+    end
+    render json: { attractions: @attractions }
+  end
 
 end
 
