943ee5c5c28cc713d15c17893ee207c967a9e0f9
Lele Duan <duanstar@gmail.com>
2015-02-16 18:18:02 -0600

    heroku fix

--- config/initializers/api_keys.rb
+++ config/initializers/api_keys.rb
@@ -1,5 +1,7 @@
 if Rails.env.development?
-  API_KEYS = YAML.load_file('config/api_keys_development.yml')[Rails.env].with_indifferent_access
+  API_KEYS = YAML.load(ERB.new(File.read('config/api_keys_development.yml')).result)[Rails.env].with_indifferent_access
 else
-  API_KEYS = YAML.load_file('config/api_keys.yml')[Rails.env].with_indifferent_access
+  API_KEYS = YAML.load(ERB.new(File.read('config/api_keys.yml')).result)[Rails.env].with_indifferent_access
 end
+
+GOOGLE_KEY = API_KEYS['google_key']
