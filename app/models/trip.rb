170f4185152cd73205a6f66a502ce332f2f166d7
Glenna Mowry <gmmowry@users.noreply.github.com>
2015-02-18 14:21:38 -0600

    Removed method that wasn't being used

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -13,24 +13,6 @@ class Trip
 
   validates_presence_of :origin, :destination
 
-  def find_waypoint_attraction
-    self.map_waypoints.each do |wypt|
-     @attraction_lat = wypt[:location][:k]
-     @attraction_long = wypt[:location][:D]
-    end
-    # attract = Attraction.where(longlat: {longitude: -86.80037006735802, latitude: 33.4196718782187})
-
-    attraction_hopefully = Attraction.find_by(longlat: {"type"=>"Point", "coordinates"=>[@attraction_long, @attraction_lat]})
-binding.pry
-    attraction_hopefully
-  end
-
 end
-
-
-# "k"=>33.4196718782187, "D"=>-86.80037006735802
-
-# Attraction.find_by(longlat: {"type"=>"Point", "coordinates"=>[33.4196718782187, -86.80037006]})
-# [-149.87912, 61.188839]
 
 
