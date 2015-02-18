ed9c5205a9d0002a0f56003e82c5df9faa570e76
nelltaylor <hj.nell.taylor@gmail.com>
2015-02-17 21:49:24 -0600

    Nav styling in progress

--- app/assets/javascripts/map.js
+++ app/assets/javascripts/map.js
@@ -13,7 +13,7 @@ function initialize() {
 
   directionsDisplay.setMap(mapObject.map);
 
-  route = new Route(START, END, WAYPOINTS);
+  route = new Route(START, END, PITSTOP);
 }
 
 var MapObject = function(){
@@ -31,7 +31,7 @@ var MapObject = function(){
 var Route = function(start, end, waypts){
   this.start = start; // birmingham
   this.end = end; // pensacola
-  this.waypts = waypts || []
+  this.waypts = waypts || [];
 
   this.calculateRoute();
 }
@@ -40,7 +40,7 @@ Route.prototype.calculateRoute = function(){
   var request = {
     origin: this.start,
     destination: this.end,
-    waypoints: this.waypts,
+    waypoints: this.pitstop,
     optimizeWaypoints: true,
     travelMode: google.maps.TravelMode.DRIVING
   };
